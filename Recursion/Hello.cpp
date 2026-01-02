d4b4c090811cfbc50618d31b13ec91a37a5e3893
Naval <navalnda23@gmail.com>
2026-01-02 22:14:37 +0530

    recursion

--- Recursion/Hello.cpp
+++ Recursion/Hello.cpp
@@ -73,14 +73,12 @@ using namespace std;
 //     }
 // }
 
-
 // int factorial(int n) {
 //     if (n == 0 || n == 1) {
 //         return 1;              // base case
 //     }
 //     return n * factorial(n - 1);  // backtracking
 // }
-
 
 // void factorial(int n, int result) {
 //     if (n == 0 || n == 1) {
@@ -91,6 +89,68 @@ using namespace std;
 //     factorial(n - 1, result);
 // }
 
+// void reverse(vector<int> &v, int i, int j)
+// {
+//     if (i >= j) return;
+//     swap(v[i], v[j]);
+//     reverse(v, i + 1, j - 1);
+// }
+
+// void reverseno(int no,int newno){
+
+//     if(no<=0) { cout<<newno; return;}
+//     int lastdigit=no%10;
+//     no=no/10;
+//     newno=newno*10+lastdigit;
+//     reverseno(no,newno);
+// }
+
+// void OccurrenceOfno(vector<int>no, int target, int count,int stindex)
+// {
+//     if (stindex>=no.size())
+//     {
+//         cout<<count;
+//         return ;
+//     }
+//     if(no[stindex]==target){
+//         count++;
+//     }
+//     OccurrenceOfno(no,target,count,stindex+1);
+// }
+
+// void minElement(vector<int>&no,int small,int stIndex){
+
+//     if(stIndex==no.size()) {cout<<small; return ;}
+//     if(no[stIndex]<=small){
+//         small=no[stIndex];
+//     }
+//     //   small = min(small, no[stIndex]);
+//     minElement(no,small,stIndex+1);
+// }
+
+
+// void Palindrome(string name,int st,int end){
+//     if(st>=end){
+//         cout<<"palindrome";
+//         return;
+//     }
+//     if(name[st]!=name[end]){
+//         cout<<"not pal";
+//         return;
+//     }
+//     Palindrome(name,st+1,end-1);
+// }
+
+//print all elements
+
+void PrintElem(vector<int>&no,int size){
+if(size==no.size())
+{
+    return;
+}
+cout<<no[size]<<endl;
+PrintElem(no,size+1);
+}
 
 int main()
 {
@@ -102,6 +162,54 @@ int main()
     // cout<<print(1, 10);
     // cout << factorial(5);
     // factorial(5, 1);
+    // string s="hello";
+    //  reverse(s,0,s.size()-1);
+    // cout<<s;
+    // vector<int> v = {1, 2, 3, 4, 5};
+    // reverse(v, 0, v.size() - 1);
+    // for (auto x : v)
+    // {
+    //     cout << x << endl;
+    // }
+
+    // int no = 12354;
+    // int newno = 0;
+    // while (no > 0)
+    // {
+    //     int lastdigit = no % 10;
+    //     newno = newno * 10 + lastdigit;
+    //     no=no / 10;
+    // }
+    // cout << newno;
+    // reverseno(no,newno);
+
+    // no of times no occur
+
+    // vector<int>no={1,2,15,3,6,4,16,0};
+    // int target = 1;
+    // int count = 0;
+    // int stindex=0;
+
+    // OccurrenceOfno(no, target, count,stindex);
+
+    // int small=INT_MAX;
+    // int stindex=0;
+
+    // minElement(no,small,stindex);
+
+    //palindrome
+    // string name="aba";
+    // int st=0;
+    // int end=name.size()-1;
+    // Palindrome(name,st,end);
+
+    
+    vector<int>no={1,2,15,3,6,4,16,0};
+    int size=0;
+    PrintElem(no,size);
+
+
+
 
     return 0;
 }
