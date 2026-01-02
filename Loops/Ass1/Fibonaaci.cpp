3e56c14321221220a29cb72464907818c704d491
Naval <navalnda23@gmail.com>
2026-01-02 22:32:03 +0530

    chnges

--- Loops/Ass1/Fibonaaci.cpp
+++ Loops/Ass1/Fibonaaci.cpp
@@ -23,3 +23,27 @@ int main(){
     
     return 0;
 }
+
+
+
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int first=0;
+//         int second=1;
+//         int i=2;
+//         int last=0;
+
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+
+//         while(i<=n){
+//             last=first+second;
+//             first=second;
+//             second=last;
+//             i++;
+//         }
+//         return last;
+//     }
+// };
